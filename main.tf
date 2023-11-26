e94099fd7dcc573656794d24dde78b9fdf43e55a
greg <greg@lucidsolutions.co.nz>
2023-11-26 08:41:43 +0000

    add teamcity build agent
    git-svn-id: https://svn.lucidsolutions.co.nz/svn/config/trunk@1344 bc274cf2-e408-4b91-9ffa-313efe910d3e

---
+++ main.tf
@@ -0,0 +1,68 @@
+terraform {
+  required_version = "~> 1.5.0"
+}
+/*
+    Jetbrains Teamcity Build Agent (deployed as a container on Flatcar Linux).
+
+    Multiple instances of this VM can be deployed (up to three are expected)
+
+    The build agent is considered completely disposable. It has extra storage,
+    but that is temporary space for builds.
+
+    see:
+      - https://hub.docker.com/r/jetbrains/teamcity-agent/
+*/
+module "khaki" {
+    source        = "c:\\dev\\lucid\\terraform-vm-proxmox"
+#  source        = "lucidsolns/proxmox/vm"
+#  version       = ">= 0.0.9"
+  vm_count      = 2
+  vm_id         = 145
+  name          = "khaki.lucidsolutions.co.nz"
+  description   = <<-EOT
+      Jetbrains Teamcity Build Agent running as a container on Flatcar Linux
+  EOT
+  startup       = "order=150"
+  tags          = ["flatcar", "jetbrains", "teamcity", "build-agent", "development"]
+  pm_api_url    = var.pm_api_url
+  target_node   = var.target_node
+  pm_user       = var.pm_user
+  pm_password   = var.pm_password
+  template_name = "flatcar-production-qemu-3602.2.1"
+  butane_conf   = "${path.module}/jetbrains-teamcity-build-agent.bu.tftpl"
+  memory        = 8192
+  networks      = [{ bridge = var.bridge, tag = 120 }]
+  disks         = [
+    // The flatcar EFI/boot/root/... template disk. This is a placeholder to
+    // stop the proxmox provider from getting too confused.
+    {
+      slot    = 0
+      type    = "scsi"
+      size    = "0K" # no size, we don't want the template resized
+      storage = "local"
+      format  = "qcow2"
+    },
+
+    // A non-persistent sparse disk for swap, this is /dev/vda in the VM
+    {
+      slot    = 1
+      type    = "virtio"
+      storage = "vmdata" # hack, this must be 'present'
+      size    = "4G" # hack, this must be present
+      format  = "raw"
+      discard = "on" # enable 'trim' support, as ZFS supports this
+    },
+
+    // A non-persisten data disk to be mounted on /opt/
+    //
+    {
+      slot    = 2
+      type    = "virtio"
+      storage = "vmdata" # hack, this must be 'present'
+      size    = "32G" # hack, disable trying to size the volume
+      format  = "raw" # default
+      discard = "on" # enable 'trim' support, as ZFS supports this
+    }
+  ]
+}
+
---
+++ variables.tf
@@ -0,0 +1,42 @@
+/*
+ * The API requires credentials. Use an API key (c.f. username/password), by going to the
+ * web UI 'Datacenter' -> 'Permissions' -> 'API Tokens' and create a new set of credentials.
+ *
+*/
+variable "pm_api_url" {
+  description = "The proxmox api endpoint"
+  default     = "https://proxmox:8006/api2/json"
+}
+
+//
+// see
+//  - https://thenewstack.io/automate-k3s-cluster-installation-on-flatcar-container-linux/
+//
+variable "target_node" {
+  description = "The name of the proxmox-ve node to provision the VM on"
+  type        = string
+}
+
+
+variable "pm_user" {
+  description = "A username for password based authentication of the Proxmox API"
+  type        = string
+  default     = "root@pam"
+}
+
+variable "pm_password" {
+  description = "A password for password based authentication of the Proxmox API"
+  type        = string
+  sensitive   = true
+  default     = ""
+}
+
+variable "bridge" {
+  default = "vmbr0"
+  type=string
+}
+
+variable "network_tag" {
+  default = 0
+  type = number
+}
